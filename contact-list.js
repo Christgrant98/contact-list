f333400cdebd88e114c82a4d4618a7fad57e6631
Christian Garcerant <christian.garcerant@uac.edu.co>
2023-06-19 19:52:53 -0500

    second task finished

--- contact-list.js
+++ contact-list.js
@@ -1,23 +1,35 @@
-let contactList = ["John Smith", "Alice Johnson", "Michael Davis", "Emily Brown"];
-console.log(contactList);
+let contactList = [];
 
-function addNewContact(name) {
-  contactList.push(name);
+function addNewContact(id, nombres, apellidos, telefono, ubicaciones, ciudad, direccion) {
+  const newContact = {
+    id: id,
+    nombres: nombres,
+    apellidos: apellidos,
+    telefono: telefono,
+    ubicaciones: ubicaciones,
+    ciudad: ciudad,
+    direccion: direccion
+  };
+
+  contactList.push(newContact);
   console.log('New contact added');
 }
 
-addNewContact("David Wilson");
-let newContactList = contactList;
-console.log("My contacts are:", newContactList);
+addNewContact(1, "Christ", "Grant", "3013740202", ["Home", "site"], "Barranquilla", "123 false street");
+console.log("My contacts are:", contactList);
 
-function deleteContact(name) {
-  const index = newContactList.findIndex(contact => contact === name);
+function deleteContact(id) {
+  const index = contactList.findIndex(contact => contact.id === id);
   if (index !== -1) {
-    newContactList.splice(index, 1);
-    console.log(`Contact "${name}" successfully removed.`);
+    const deletedContact = contactList.splice(index, 1);
+    console.log(`Contact with id "${id}" successfully removed.`);
+    return deletedContact[0];
+  } else {
+    console.log(`Contact with id "${id}" not found.`);
+    return null;
   }
 }
 
-deleteContact("Alice Johnson");
-let deletedContactList = newContactList;
-console.log("The updated contact list is:", deletedContactList);
+const deletedContact = deleteContact(1);
+console.log("Deleted contact:", deletedContact);
+console.log("The updated contact list is:", contactList);
